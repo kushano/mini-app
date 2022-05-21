4b140d4c0ee9cea9ec7c0ed0abe1dff3a7c6388c
kushano <43448851+kushano@users.noreply.github.com>
2022-05-21 20:18:20 +0500

    no messagelist now

--- src/components/ChatComp.js
+++ src/components/ChatComp.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { FixedLayout, Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
-import MessageList from '../components/MessageList';
+import { FixedLayout, Panel, PanelHeader, PanelHeaderBack, Spacing } from '@vkontakte/vkui';
+import MessageComp from '../components/MessageComp';
 import MessageBox from '../components/MessageBox';
 
 function ChatComp() {
-    const [msgs, setMsgs] = useState([])
+    const [messages, setMessages] = useState([
+        { id: 1, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 2, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 3, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 4, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 5, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 6, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 7, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 8, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
+        { id: 9, sender_name: 'Lev', text: 'Я щас вымок весь под дождём(' },
+        { id: 10, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' }
+    ])
 
     // const addNewMsg = (e) => {
     //     e.preventDefault()
@@ -16,9 +27,15 @@ function ChatComp() {
     //     setMsgs(...msgs, newMsg)
     // }
     
+    const messagesList = messages.map(message =>
+        <MessageComp message={message} key={message.id} />
+    )
+
     return (
         <div>
-            <MessageList />
+            {messagesList}
+
+            <Spacing size={38} />
 
             <FixedLayout filled vertical='bottom'>
                 <MessageBox/>
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,40 +1,26 @@
-import React, { useEffect, useState, useRef } from 'react';
-import MessageComp from '../components/MessageComp';
+// import React, { useEffect, useState, useRef } from 'react';
+// import MessageComp from '../components/MessageComp';
 
-import { Spacing } from '@vkontakte/vkui';
+// import { Spacing } from '@vkontakte/vkui';
 
-function MessageList(props) {
-    const messageList = React.createRef();
+// function MessageList(props) {
+//     const messageList = React.createRef();
 
-    useEffect(() => {
-        messageList.current.scrollTop = messageList.current.scrollHeight
-    }, [props, messageList]);
+//     useEffect(() => {
+//         messageList.current.scrollTop = messageList.current.scrollHeight
+//     }, [props, messageList]);
 
+//     const messagesList = messages.map(message =>
+//         <MessageComp message={message} key={message.id} />
+//     )
 
+//     return (
+//         <section className="messagesList" ref={messageList}>
+//             {/* {messagesList} */}
+//             <MessageComp message={messages} key={messages.id}/>
+//             <Spacing size={36} />
+//         </section>
+//     );
+// }
 
-    const [messages, setMessages] = useState([
-        { id: 1, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 2, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 3, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 4, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 5, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 6, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 7, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 8, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' },
-        { id: 9, sender_name: 'Lev', text: 'Я щас вымок весь под дождём(' },
-        { id: 10, sender_name: 'Vladislav', text: 'ой, блять заебался, сделал коммит, он не сделался, сделал сейчас хуйни, рестор только к началу, по сути' }
-    ])
-
-    const messagesList = messages.map(message =>
-        <MessageComp message={message} key={message.id} />
-    )
-
-    return (
-        <section className="messagesList" ref={messageList}>
-            {messagesList}
-            <Spacing size={36} />
-        </section>
-    );
-}
-
-export default MessageList;
+// export default MessageList;
